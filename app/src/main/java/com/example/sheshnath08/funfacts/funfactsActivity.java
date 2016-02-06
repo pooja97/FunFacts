1fbdf9e7c5d99d764ee52c3e36f3a2410c512d3b
pooja97 <poojayadav121997@gmail.com>
2016-02-06 14:17:51 +0530

    Modified one

--- app/src/main/java/com/example/sheshnath08/funfacts/funfactsActivity.java
+++ app/src/main/java/com/example/sheshnath08/funfacts/funfactsActivity.java
@@ -53,12 +53,6 @@ public class funfactsActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_funfacts, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
